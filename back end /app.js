5c67d5142f99021a03a672b91ef3ce8723f040e9
Caroline Villain <caroline.villain77@gmail.com>
2023-04-10 10:44:07 +0200

    modif 2

--- back end /app.js
+++ back end /app.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodbsrv://carovil:TcD7IliH1elfQXOS@cluster0.vnwzmy4.mongodb.net/test?retryWrites=true&w=majority',
-  {   useNewUrlParser: true, 
-      useUnifiedTopology: true})
+
+
+mongoose.connect('mongodb+srv://carovil:TcD7IliH1elfQXOS@cluster0.vnwzmy4.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true, 
+    useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie!'))
   .catch(() => console.log('Connexion à MongoDB échouée!'));
 
 const express = require('express');
 
-const app = express();  
+const app = express(); 
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,26 +21,22 @@ app.use((req, res, next) => {
     next();
 });
 
+const Thing = require('./models/thing');
+
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
 });
    
 module.exports = app;
