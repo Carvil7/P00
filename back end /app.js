1284f231dcfa3f208b9faebcf3d1baf489f2f94f
Caroline Villain <caroline.villain77@gmail.com>
2023-04-14 09:30:02 +0200

    modif

--- back end /app.js
+++ back end /app.js
@@ -1,14 +1,13 @@
+const express = require('express');
 const mongoose = require('mongoose');
 
-
+const Thing = require('./models/thing');
 
 mongoose.connect('mongodb+srv://carovil:TcD7IliH1elfQXOS@cluster0.vnwzmy4.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true, 
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie!'))
   .catch(() => console.log('Connexion à MongoDB échouée!'));
-
-const express = require('express');
 
 const app = express(); 
 
@@ -21,8 +20,6 @@ app.use((req, res, next) => {
     next();
 });
 
-const Thing = require('./models/thing');
-
 app.post('/api/stuff', (req, res, next) => {
   delete req.body._id;
   const thing = new Thing({
@@ -33,10 +30,16 @@ app.post('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 app.use('/api/stuff', (req, res, next) => {
   Thing.find()
     .then(things => res.status(200).json(things))
     .catch(error => res.status(400).json({ error }));
 });
-   
+
 module.exports = app;
