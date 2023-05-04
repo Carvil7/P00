c564e2a14914e3d304ca931f808346f6ae28ef08
Caroline Villain <caroline.villain77@gmail.com>
2023-05-05 01:28:52 +0200

    fin

--- back end /app.js
+++ back end /app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+
+const path = require('path');
 
 mongoose.connect('mongodb+srv://carovil:TcD7IliH1elfQXOS@cluster0.vnwzmy4.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true, 
@@ -21,5 +24,8 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
